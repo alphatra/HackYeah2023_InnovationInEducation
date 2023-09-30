f843b9709c09dd55b9f4a048b4b86759216f65fc
MrNikrutix <mrnikrutix@gmail.com>
2023-09-30 22:27:11 +0200

    backrefactor

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException, Path
-from sqlalchemy import create_engine, Column, Integer, Text, ForeignKey, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session, relationship
-from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List
+from .models.all import *
+from .schemas.all import *
+
 
 app = FastAPI()
 
@@ -24,64 +24,6 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Model items
-Base = declarative_base()
-
-class Item(Base):
-    __tablename__ = "items"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    question = Column(String(255), index=True)
-    answers = relationship("Answer", back_populates="quest")
-
-class Answer(Base):
-    __tablename__ = "answers"
-    answer_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    answer = Column(String(255), index=True)
-    question_id = Column(Integer, ForeignKey("items.id", ondelete="CASCADE", onupdate="CASCADE"))
-    technical = Column(Integer)
-    medical = Column(Integer)
-    lingual = Column(Integer)
-    art = Column(Integer)
-
-    quest = relationship("Item", back_populates="answers")
-
-class Answer_sch(BaseModel):
-    answer_id: int
-    answer: str
-    question_id: int
-    class Config:
-        orm_mode = True
-
-class Item_sch(BaseModel):
-    question: str
-    answers: list[str]
-    class Config:
-        orm_mode = True
-
-class AnswerResponse(BaseModel):
-    answer_id: int
-    answer: str
-
-class ItemResponse(BaseModel):
-    id: int
-    
-    question: str
-    answers: List[AnswerResponse]
-    class Config:
-        orm_mode = True
-
-class Survey(BaseModel):
-    question_id: int
-    answer_id: int
-
-
-class ApiForAi(BaseModel):
-    answer_id: int
-    technical: int
-    medical: int
-    lingual: int
-    art: int
-   
 
 @app.on_event("startup")
 def startup_db_client():
@@ -142,7 +84,7 @@ def get_item(answer_id: int = Path(..., title="Answer ID", description="The ID o
     if answer is None:
         raise HTTPException(status_code=404, detail="Answer not found")
 
-    # Create an instance of ApiForAi with the answer data
+
     api_for_ai_response = ApiForAi(
         answer_id=answer.answer_id,
         technical=answer.technical,
